ad5f9b0bfea84a9e368557e094e86352a6a4dda7
Aniruddha Mukherjee <aniruddha.mukherji@outlook.com>
2024-03-05 23:04:00 +0530

    build script for making zip

---
+++ build.py
@@ -0,0 +1,32 @@
+import re
+import os
+import sys
+import zipfile
+
+if len(sys.argv) < 2:
+    print("Usage: python build.py <version_string>")
+    sys.exit(1)
+
+excluded_file_paths = {r"^.git", r"^build", r"^.prettierrc", r"^.git",
+                       r"^ts", r"^node_modules", r"LICENSE", r"^package", r"^readme", r"^tsconfig", r".vscode"}
+
+
+def shouldBeAdded(file_path):
+    for each_path in excluded_file_paths:
+        if (re.match(each_path, file_path)):
+            return False
+    return True
+
+
+print(f"Version string: {sys.argv[1]}")
+with zipfile.ZipFile(f"./build/fontonic-{sys.argv[1]}.zip", "w") as zip_file:
+    for root, _, files in os.walk("./"):
+        for file in files:
+            full_path = os.path.join(root, file)
+            archive_path = os.path.relpath(full_path, "./")
+            if (shouldBeAdded(archive_path)):
+                print(f"Added: {archive_path}")
+                zip_file.write(full_path, archive_path)
+
+
+print("Zip created successfully.")
