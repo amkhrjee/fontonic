3ec653a3124e355f2ca25c8676ee4b786fb9fb65
amkhrjee <aniruddha.mukherji@outlook.com>
2024-08-12 19:55:11 +0530

    fix: navigation between pages

--- ts/popup.ts
+++ ts/popup.ts
@@ -3,13 +3,18 @@ const supportButton = document.getElementById("support-btn");
 const homePage = document.getElementById("home-page");
 const settingsPage = document.getElementById("settings-page");
 const wrapper = document.getElementById("wrapper");
+const supportPage = document.getElementById("support-page");
 
 // by default these extra pages are unmounted
 settingsPage.remove();
+supportPage.remove();
+
 settingsButton.addEventListener("click", () => {
   if (settingsButton.textContent.charAt(0) === "S") {
     settingsButton.textContent = "Go back";
-    homePage.remove();
+    if (supportButton.textContent.includes("<")) supportPage.remove();
+    else homePage.remove();
+    supportButton.textContent = "❤ Support";
     wrapper.appendChild(settingsPage);
   } else {
     settingsButton.textContent = "Settings";
@@ -17,8 +22,17 @@ settingsButton.addEventListener("click", () => {
     wrapper.appendChild(homePage);
   }
 });
+
 supportButton.addEventListener("click", () => {
-  supportButton.textContent.includes("❤")
-    ? (supportButton.textContent = "<- Go back")
-    : (supportButton.textContent = "❤ Support");
+  if (supportButton.textContent.includes("❤")) {
+    supportButton.textContent = "<- Go back";
+    if (settingsButton.textContent.includes("G")) settingsPage.remove();
+    else homePage.remove();
+    settingsButton.textContent = "Settings";
+    wrapper.appendChild(supportPage);
+  } else {
+    supportButton.textContent = "❤ Support";
+    supportPage.remove();
+    wrapper.appendChild(homePage);
+  }
 });
